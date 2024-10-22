41e21f2a67d367e370cfeb88d2b4b383940ed440
Ethan Heckart <heckart@proton.me>
2024-10-22 17:34:17 -0500

    added GET function initial minimal implementation for the GET handler, function comment updates, and a small addition to the POST handler

--- backend/src/api_backend.cpp
+++ backend/src/api_backend.cpp
@@ -5,6 +5,16 @@
 
 #include <iostream>
 
+void handle_get(web::http::http_request request) {
+  // creating a dummy object for now
+  // TODO: update this once we know whats needed
+  web::json::value userInfo;
+  userInfo[U("name")] = web::json::value::string(U("2% Milk"));
+  userInfo[U("price")] = web::json::value::number(3);
+
+  request.reply(web::http::status_codes::OK, userInfo);
+}
+
 void handle_post(web::http::http_request request) {
   request.extract_json()
       .then([=](web::json::value jsonObject) {
@@ -12,6 +22,7 @@ void handle_post(web::http::http_request request) {
           // Extract data from the request body
           // TODO: find out what data needs to be rec'd and update this
           std::string name = jsonObject[U("name")].as_string();
+          int price = jsonObject[U("price")].as_integer();
 
           // log it...probably can delete
           std::cout << L"found Name: " << name << std::endl;
--- backend/src/api_backend.h
+++ backend/src/api_backend.h
@@ -5,11 +5,21 @@
 #include <cpprest/json.h>
 
 /**
+ * @brief Handles GET requests to collect and return information from a DB
+ *
+ * @param request The HTTP request object containing the DB request
+ *
+ * @return Nothing. Responds to the client with the requested information.
+ */
+void handle_get(web::http::http_request request);
+
+/**
  * @brief Handles POST requests to receive user information in JSON format.
  *
- * @param The HTTP request object containing the JSON payload.
+ * @param request The HTTP request object containing the JSON payload.
  *
- * @return Nothing.
+ * @return Nothing. Responds to the client that info has been received or that
+ * an error as occured.
  */
 void handle_post(web::http::http_request request);
 
