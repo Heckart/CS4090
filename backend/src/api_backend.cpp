b086051e3b3a73843904926da4f0f7ca9e09d508
Ethan Heckart <heckart@proton.me>
2024-11-14 19:27:55 -0600

    added JSON examples to api function comments This could be helpful!

--- backend/src/api_backend.cpp
+++ backend/src/api_backend.cpp
@@ -7,16 +7,6 @@
 
 #include <iostream>
 
-/*
-  enum class OrderStatus {
-    ORDERING,
-    CONFIMRED,
-    DISPATCHED,
-    DELIVERED,
-    CANCELLED
-  }
-   */
-
 void handle_request(web::http::http_request request) {
   auto path = web::uri::decode(request.relative_uri().path());
 
@@ -38,6 +28,20 @@ void handle_start_order(web::http::http_request request) {
   request.extract_json()
       .then([=](web::json::value jsonObject) {
         try {
+
+	  /** Example of what JSON object strcuture this is looking for:
+	      {
+	        "userID": "01272"
+		"addressInfo": {
+		    addressPrimary: "123 Market Street",
+		    addressSecondary: "Apt 301",
+		    city: "Rolla",
+		    state: "MO",
+		    zipCode: "65401"
+		    }
+	       }
+	   */
+	  
           std::string userID = utility::conversions::to_utf8string(
               jsonObject[U("userID")].as_string());
           web::json::value addressInfo = jsonObject[U("addressInfo")];
@@ -56,6 +60,37 @@ void handle_start_order(web::http::http_request request) {
           response[U("businessInfo")] = businessInfo;
 
           // Send response to the client
+
+          /** Example of what JSON object structure this replies with:
+              {
+               "orderID": "NCS-263-A",
+	       "businessInfo": [
+                  {
+                    "businessID": "12345",
+                    "businessName": "Example Business",
+                    "addressInfo": {
+                                     "addressPrimary": "123 Main St",
+                                     "addressSecondary": "Suite 100",
+                                     "city": "Springfield",
+                                     "state": "IL",
+                                     "zipCode": "62701"
+                                     }
+                  },
+                  {
+                    "businessID": "67890",
+                    "businessName": "Another Business",
+                    "addressInfo": {
+                                     "addressPrimary": "456 Elm St",
+                                     "addressSecondary": "",
+                                     "city": "Greenfield",
+                                     "state": "WI",
+                                     "zipCode": "53220"
+                                   }
+                  }
+              ]
+              }
+           */
+
           request.reply(web::http::status_codes::OK, response);
         } catch (const std::exception &e) {
           request.reply(web::http::status_codes::BadRequest,
@@ -69,6 +104,15 @@ void handle_select_business(web::http::http_request request) {
   request.extract_json()
       .then([=](web::json::value jsonObject) {
         try {
+
+          /** Example of what JSON object structure this is looking for:
+              {
+	        "userID": "38284",
+		"orderID": "38DBC-23S",
+		"businessID": "28382"
+	      }
+           */
+
           // Extract userID, orderID, and businessID from the request JSON
           std::string userID = utility::conversions::to_utf8string(
               jsonObject[U("userID")].as_string());
@@ -81,7 +125,7 @@ void handle_select_business(web::http::http_request request) {
 
           // Fetch available items for the selected business from the database
           web::json::value itemsArray = fetch_items_from_db(businessID);
-
+          
           // Construct response JSON
           web::json::value response;
           response[U("businessID")] = web::json::value::string(
@@ -89,6 +133,25 @@ void handle_select_business(web::http::http_request request) {
           response[U("items")] = itemsArray;
 
           // Send response to the client
+
+          /** Example of what JSON object structure this replies with:
+              {
+                "businessID": "29432",
+                 "items": [
+                  {
+                    "upc": 123456789012,
+                    "name": "Widget A",
+                    "price": 19.99
+                  },
+                  {
+                    "upc": 987654321098,
+                    "name": "Widget B",
+                    "price": 29.99
+                  }
+                 ]
+	       }
+           */
+
           request.reply(web::http::status_codes::OK, response);
         } catch (const std::exception &e) {
           request.reply(web::http::status_codes::BadRequest,
@@ -102,6 +165,14 @@ void handle_select_shopper(web::http::http_request request) {
   request.extract_json()
       .then([=](web::json::value jsonObject) {
         try {
+
+          /** Example of the JSON object structure this is looking for:
+              {
+	        "userID": "84828",
+		"orderID": "ABC-3284"
+	      }
+           */
+
           std::string userID = utility::conversions::to_utf8string(
               jsonObject[U("userID")].as_string());
           std::string orderID = utility::conversions::to_utf8string(
@@ -117,6 +188,26 @@ void handle_select_shopper(web::http::http_request request) {
           response[U("shoppers")] = shoppersArray;
 
           // Send response to the client
+
+          /** Example of the JSON object structure this replies with:
+             {
+	       "shoppers": [
+	                     {
+			       "shopperID": "s123",
+			       "firstName": "Alice",
+			       "lastName": "Smith",
+			       "fulfillmentTime": "2023-11-14 10:30:00"
+			     },
+			     {
+			       "shopperID": "s456",
+			       "firstName": "Bob",
+			       "lastName": "Johnson",
+			       "fulfillmentTime": "2023-11-14 11:45:00"
+			     }
+			    ]
+	    }
+           */
+
           request.reply(web::http::status_codes::OK, response);
 
         } catch (const std::exception &e) {
